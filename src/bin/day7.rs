ca91bdea3ebaa86dfd6103d40de3bb479c009e1f
d <dsandall@calpoly.edu>
2025-12-25 08:26:31 -0800

    day 7c1 (functioning for small tests bottom up alg, needs memoization)

--- src/bin/day7.rs
+++ src/bin/day7.rs
@@ -1,6 +1,10 @@
 #![feature(pattern)]
+#![feature(breakpoint)]
 use core::panic;
-use std::io::{self, BufRead, Read};
+use std::{
+    collections::{HashMap, VecDeque},
+    io::{self, BufRead, Read},
+};
 
 struct Grid {
     width: usize,
@@ -9,20 +13,14 @@ struct Grid {
 }
 
 impl Grid {
-    fn get(&self, p: (usize, usize)) -> Option<&u8> {
-        if p.0 < self.width && p.1 < self.height {
-            Some(&self.data[p.0 + p.1 * self.width])
+    fn get(&self, p: (i32, i32)) -> Option<&u8> {
+        if (0..self.width as i32).contains(&p.0) && (0..self.height as i32).contains(&p.1) {
+            Some(&self.data[p.0 as usize + p.1 as usize * self.width])
         } else {
             None
         }
     }
-    fn get_mut(&mut self, p: (usize, usize)) -> Option<&mut u8> {
-        if p.0 < self.width && p.1 < self.height {
-            Some(&mut self.data[p.0 + p.1 * self.width])
-        } else {
-            None
-        }
-    }
+
     fn disp(&self) {
         let v = self.data.chunks(self.width);
         let v = v.map(|a| a.iter().map(|a| *a as char).collect());
@@ -48,56 +46,60 @@ fn main() {
     dbg!(g.width, g.height);
     g.disp();
 
-    let mut beamfronts: Vec<(usize, usize)> = (0..g.width)
-        .filter_map(|x| {
-            if *g.get((x, 0)).unwrap() == b'S' {
-                Some((x, 0))
-            } else {
-                None
-            }
-        })
-        .collect();
+    let mut count: u64 = 0;
+    type Saved = u32;
+    let mut prev: HashMap<(usize, usize), Saved> = HashMap::new();
 
-    let mut count = 0;
-    for _ in 0..g.height - 1 {
-        let mut new_fronts = Vec::new();
+    for xterm in 0..g.width {
+        // initial possible ending point
+        let mut beamfronts: VecDeque<(i32, i32)> = VecDeque::new();
+        beamfronts.push_back((xterm as i32, (g.height - 1) as i32));
 
-        for (x, y) in beamfronts.iter() {
-            let p = (*x, *y + 1);
+        dbg!("new start point");
+        dbg!(beamfronts.iter().peekable().peek());
 
-            match g.get(p) {
-                Some(b'^') => {
-                    let r = (*x + 1, *y + 1);
-                    let l = (*x - 1, *y + 1);
-                    for n in [r, l] {
-                        if g.get_mut(n).is_some() {
-                            *g.get_mut(n).unwrap() = b'|';
-                            new_fronts.push(n);
-                        } else {
-                            panic!();
-                        }
+        // parse all possible paths to the end point
+        while beamfronts.len() != 0 {
+            let (mut x, mut y): (i32, i32) = beamfronts.pop_front().unwrap();
+
+            let paths = 1;
+            loop {
+                // check if this could be the start of a split beam
+                let (l, r) = ((x + 1, y), (x - 1, y));
+                for n in [l, r] {
+                    if g.get(n).is_some_and(|c| *c == b'^') {
+                        // record split
+                        beamfronts.push_back(n);
                     }
+                }
 
-                    // count splits
-                    count += 1;
-                }
-                Some(b'.') => {
-                    *g.get_mut(p).unwrap() = b'|';
-                    new_fronts.push(p);
-                }
-                Some(b'|') => {}
-                _ => {
-                    dbg!(*g.get(p).unwrap() as char);
-                    panic!();
+                // trace back regular beams, or terminate it
+                let up = (x, y - 1);
+                match g.get(up) {
+                    Some(b'S') => {
+                        // positive terminating condition, save to count
+                        //TODO:
+                        count += paths;
+                        if count.is_multiple_of(10000) {
+                            dbg!(count);
+                        }
+                        break;
+                    }
+                    Some(b'.') => {
+                        // trace propagation further back
+                        y -= 1;
+                    }
+                    Some(b'^') | None => {
+                        // end of path, disregard current count
+                        // no non-split beams can come from directly below a splitter
+                        break;
+                    }
+                    _ => {
+                        panic!();
+                    }
                 }
             }
         }
-
-        // Append new fronts after iteration
-        beamfronts = new_fronts;
-
-        dbg!(&beamfronts);
-        g.disp();
     }
 
     dbg!(count);
